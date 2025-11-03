10c6642732646e8fc4d2dd977d67a83fb0b87a65
ArthurGausmann <arthur.campellogausmann@gmail.com>
2025-11-02 21:37:54 -0300

    Pratica 1 refatorada

--- trabalhoPratico1.c
+++ trabalhoPratico1.c
@@ -4,7 +4,7 @@ Trabalho Pratico 1 - Arthur Campello Gausmann
 Leetcode 840.
 */
 
-bool lemonadeChange( int* bills, int billsSize ) {
+bool lemonadeChange(int* bills, int billsSize) {
     int nota5 = 0;
     int nota10 = 0;
     for ( int i = 0; i < billsSize; i++ ) {
@@ -14,14 +14,18 @@ bool lemonadeChange( int* bills, int billsSize ) {
             if ( nota5 > 0 ) {
                 nota5 -= 1;
                 nota10 += 1;
-            } else { return false; }
-        } else if ( bills[i] == 20 ) {
+            } else {
+                return false;
+            }
+        } else if (bills[i] == 20) {
             if ( nota5 > 0 && nota10 > 0 ) {
                 nota5 -= 1;
                 nota10 -= 1;
             } else if ( nota5 > 2 ) {
                 nota5 -= 3;
-            } else { return false; }
+            } else {
+                return false;
+            }
         }
     }
 
